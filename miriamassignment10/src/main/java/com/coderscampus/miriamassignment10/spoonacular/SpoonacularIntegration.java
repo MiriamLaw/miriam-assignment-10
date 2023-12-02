49334690ae7a824fb5abbc2e56f2cb92d9376541
MiriamLaw <mimsical999@yahoo.com>
2023-12-02 09:28:51 -0800

    Packages and classes
    Controller built out

--- miriamassignment10/src/main/java/com/coderscampus/miriamassignment10/spoonacular/SpoonacularIntegration.java
+++ miriamassignment10/src/main/java/com/coderscampus/miriamassignment10/spoonacular/SpoonacularIntegration.java
@@ -1,5 +1,10 @@
 package com.coderscampus.miriamassignment10.spoonacular;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpoonacularIntegration {
+	
+	
 
 }
--- miriamassignment10/src/main/java/com/coderscampus/miriamassignment10/web/MealPlannerController.java
+++ miriamassignment10/src/main/java/com/coderscampus/miriamassignment10/web/MealPlannerController.java
@@ -1,10 +1,14 @@
 package com.coderscampus.miriamassignment10.web;
 
+import java.net.URI;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.coderscampus.miriamassignment10.spoonacular.dto.DayResponse;
 import com.coderscampus.miriamassignment10.spoonacular.dto.WeekResponse;
@@ -12,21 +16,44 @@ import com.coderscampus.miriamassignment10.spoonacular.dto.WeekResponse;
 @RestController
 @RequestMapping("/mealplanner")
 public class MealPlannerController {
+	private static final String API_KEY = "0b39434da0644e9f9617f949da1fe6ad";
 	
 	@GetMapping("mealplanner/week")
-	public ResponseEntity<WeekResponse> getWeekMeals(@RequestParam String numCalories, 
+	public ResponseEntity<WeekResponse> getWeekMeals(
+			@RequestParam String numCalories, 
 			@RequestParam String diet, 
 			@RequestParam String exclusions) {
-				return null;
-		//implement logic to fetch week meals from spoonacular
+		RestTemplate restTemplate = new RestTemplate();
+		
+		URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/generate")
+									  .queryParam("apiKey", API_KEY)
+									  .queryParam("targetCalories", numCalories)
+									  .queryParam("diet", diet)
+									  .queryParam("exclude", exclusions)
+									  .build()
+									  .toUri();
+		ResponseEntity<WeekResponse> response = restTemplate.getForEntity(uri, WeekResponse.class);
+				return response;
 	}
 	
 	@GetMapping("mealplanner/day")
-	public ResponseEntity<DayResponse> getDayMeals(@RequestParam String numCalories, 
-			@RequestParam String diet, 
-			@RequestParam String exclusions) {
-				return null;
-		//implement logic to fetch day meals from spoonacular
+	public ResponseEntity<DayResponse> getDayMeals(
+			@RequestParam(value = "targetCalories", required = false) String numCalories, 
+			@RequestParam(required = false) String diet, 
+			@RequestParam(value = "exclude", required = false) String exclusions) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
+									  .queryParam("apiKey", API_KEY)
+									  .queryParam("timeFrame", "day")
+									  .queryParam("targetCalories", numCalories)
+									  .queryParam("diet", diet)
+									  .queryParam("exclude", exclusions)
+									  .build()
+									  .toUri();
+		ResponseEntity<DayResponse> response = restTemplate.getForEntity(uri, DayResponse.class);						  
+				return response;
 	}
 
 }
