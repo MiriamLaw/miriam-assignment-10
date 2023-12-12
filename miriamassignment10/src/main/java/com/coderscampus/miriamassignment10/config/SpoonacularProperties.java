208f0d74631c9006dcefca7ecec6fb36796e23fc
MiriamLaw <mimsical999@yahoo.com>
2023-12-11 16:57:44 -0800

    Integration

--- miriamassignment10/src/main/java/com/coderscampus/miriamassignment10/config/SpoonacularProperties.java
+++ miriamassignment10/src/main/java/com/coderscampus/miriamassignment10/config/SpoonacularProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "spoonacular.api")
+@ConfigurationProperties(prefix = "api.spoonacular.com")
 public class SpoonacularProperties {
 	
 	private String baseUrl;
--- miriamassignment10/src/main/java/com/coderscampus/miriamassignment10/spoonacular/SpoonacularIntegration.java
+++ miriamassignment10/src/main/java/com/coderscampus/miriamassignment10/spoonacular/SpoonacularIntegration.java
@@ -1,10 +1,20 @@
 package com.coderscampus.miriamassignment10.spoonacular;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class SpoonacularIntegration {
 	
+	@Value("${spoonacular.urls.base}")
+	private String baseUrl;
+	
+	@Value("${spoonacular.urls.mealplan}")
+	private String mealPlan;
+	
+	@Value("${spoonacular.urls.key}")
+	private String key;
+
 	
 
 }
